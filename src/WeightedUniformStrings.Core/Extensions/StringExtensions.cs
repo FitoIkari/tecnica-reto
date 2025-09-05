584e0130e1397dce4ccf7cd5666f343982dff150
FitoIkari <chris_fito@hotmail.com>
2025-09-05 13:13:42 -0600

    Prueba de lógica resulta

---
+++ src/WeightedUniformStrings.Core/Extensions/StringExtensions.cs
@@ -0,0 +1,15 @@
+﻿namespace WeightedUniformStrings.Core.Extensions
+{
+    public static class StringExtensions
+    {
+        private static string weigths = "abcdefghijklmnopqrstuvwxyz";
+        public static int CalcWeigth(this string s)
+        {
+            int weigth = 0;
+            foreach (var x in s)
+                weigth += s.IndexOf(x) + 1;
+
+            return weigth;
+        }
+    }
+}
--- src/WeightedUniformStrings.Core/WeightedUniformStringSolver.cs
+++ src/WeightedUniformStrings.Core/WeightedUniformStringSolver.cs
@@ -1,9 +1,40 @@
-﻿namespace WeightedUniformStrings.Core;
+﻿using WeightedUniformStrings.Core.Extensions;
+
+namespace WeightedUniformStrings.Core;
 
 public class WeightedUniformStringSolver
 {
     public List<string> WeightedUniformStrings(string s, List<int> queries)
     {
-        return new List<string>();
+        if (string.IsNullOrEmpty(s)) return queries.Select(x => $"No").ToList();
+        
+        Dictionary<string, int> U = new();
+
+        char lastChar = s[0];
+        string aux = s[0].ToString();
+
+
+        for (int i = 1; i < s.Length; i++)
+        {
+            char c = s[i];
+            if (c == lastChar)
+            { 
+                aux += c;
+                continue;
+            }
+
+            if(U.ContainsKey(aux))
+                U[aux] += aux.CalcWeigth();
+            else
+                U.Add(aux, aux.CalcWeigth());
+            aux = c.ToString();
+            lastChar = c;
+        }
+
+
+
+        return queries
+            .Select(x => U.Any(u => u.Value == x) ? "Yes" : "No")
+            .ToList();
     }
 }
